d16a824aa2e59d032efa1d634e22402d0898938d
Ruben M. Damião <rmdamiao@gmail.com>
2024-09-27 01:51:45 -0300

    launch local handles node

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"context"
 	"os"
 	"strings"
+
+	"github.com/freehandle/breeze/crypto"
+	"github.com/freehandle/breeze/middleware/social"
+	"github.com/freehandle/breeze/util"
+	"github.com/freehandle/handles/attorney"
 )
+
+const (
+	handlesProtocolCode = 1
+	breezeProtocolCode  = 0
+	notarypath          = ""
+	blocksPath          = ""
+	blocksName          = "chain"
+)
+
+func launchGenesis(ctx context.Context, credentials crypto.PrivateKey, listeners []chan []byte) (*social.LocalBlockChain[*attorney.Mutations, *attorney.MutatingState], error) {
+	genesis := attorney.NewGenesisState(notarypath)
+	IO, err := util.OpenMultiFileStore(blocksPath, blocksName)
+	if err != nil {
+		return nil, err
+	}
+	cfg := &social.LocalChainConfig[*attorney.Mutations, *attorney.MutatingState]{
+		Credentials:  credentials,
+		ProtocolCode: handlesProtocolCode,
+		Interval:     1,
+		Listeners:    listeners,
+		Genesis:      genesis,
+	}
+	local, err := social.OpenChain[*attorney.Mutations, *attorney.MutatingState](IO, cfg)
+	return local, err
+}
 
 func main() {
 
